7dc34d718377a68a65aea24bbd2b448521c22388
Hazzic <s.bronzebeard@gmail.com>
2023-03-07 20:56:24 +0000

    started working on the OOP function JS code

---
+++ starter/index.js
@@ -0,0 +1,81 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+const idList = [];
+const teamMembers = [];
+
+const appMenu = () => {
+    function createTeam() {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "Which type of team member would you like to add?",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "I don't want to add any more team members"
+                ]
+            }
+        ]).then(userChoice => {
+            if(userChoice.memberChoice === "Engineer") {
+                // TODO Add Engineer function
+            } else if(userChoice.memberChoice === "Intern") {
+                // TODO Add Intern function
+            } else {
+                // TODO Add buildTeam function
+            }
+        })
+    }
+
+    function createManager() {
+        console.log("Please build your team:");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the team manager's name?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the team manager's ID?",
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the team manager's email?",
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is the team manager's office number?",
+            },
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamMembers.push(manager);
+            idList.push(answers.managerId);
+            createTeam();
+        })
+    }
+    createManager();
+}
+
+appMenu();
